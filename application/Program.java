891c71271fe14cb975bcb448695ca9ac08682a1e
Lucas Amorim Alves <amorim.fitness@gmail.com>
2023-08-24 17:11:33 -0300

    Handling exceptions and clearing screen

--- application/Program.java
+++ application/Program.java
@@ -24,16 +24,29 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		
 		while (true) {
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 			
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 		}
+			catch (ChessException e){
+				System.out.println(e.getMessage());
+				sc.nextLine();
+	}
+			catch (InputMismatchException e){
+				System.out.println(e.getMessage());
+				sc.nextLine();
+	}
 	}
 }
+}
+		
